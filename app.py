fa0e92f5d4758ec536eb19b5c64dd6b6b6393a82
K-SOHAIL <kamransohail8910@gmail.com>
2025-11-23 19:09:51 +0530

    view 10 books

--- app.py
+++ app.py
@@ -8,60 +8,57 @@ from books_recommender.config.configuration import AppConfiguration
 from books_recommender.pipeline.training_pipeline import TrainingPipeline
 from books_recommender.exception.exception_handler import AppException
 
-
 class Recommendation:
-    def __init__(self,app_config = AppConfiguration()):
+    def __init__(self, app_config=AppConfiguration()):
         try:
-            self.recommendation_config= app_config.get_recommendation_config()
+            self.recommendation_config = app_config.get_recommendation_config()
         except Exception as e:
             raise AppException(e, sys) from e
 
-
-    def fetch_poster(self,suggestion):
+    def fetch_poster(self, suggestion):
         try:
             book_name = []
             ids_index = []
             poster_url = []
-            book_pivot =  pickle.load(open(self.recommendation_config.book_pivot_serialized_objects,'rb'))
-            final_rating =  pickle.load(open(self.recommendation_config.final_rating_serialized_objects,'rb'))
+            book_pivot = pickle.load(open(self.recommendation_config.book_pivot_serialized_objects, 'rb'))
+            final_rating = pickle.load(open(self.recommendation_config.final_rating_serialized_objects, 'rb'))
 
+            # Collect book names from suggestion indices
             for book_id in suggestion:
                 book_name.append(book_pivot.index[book_id])
 
-            for name in book_name[0]: 
+            # Get indices of final_rating by title
+            for name in book_name[0]:
                 ids = np.where(final_rating['title'] == name)[0][0]
                 ids_index.append(ids)
 
+            # Get poster URLs
             for idx in ids_index:
                 url = final_rating.iloc[idx]['image_url']
                 poster_url.append(url)
 
             return poster_url
-        
         except Exception as e:
             raise AppException(e, sys) from e
-        
 
-
-    def recommend_book(self,book_name):
+    def recommend_book(self, book_name):
         try:
             books_list = []
-            model = pickle.load(open(self.recommendation_config.trained_model_path,'rb'))
-            book_pivot =  pickle.load(open(self.recommendation_config.book_pivot_serialized_objects,'rb'))
+            model = pickle.load(open(self.recommendation_config.trained_model_path, 'rb'))
+            book_pivot = pickle.load(open(self.recommendation_config.book_pivot_serialized_objects, 'rb'))
             book_id = np.where(book_pivot.index == book_name)[0][0]
-            distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
+            # n_neighbors=11: 1 for the selected book, 10 recommendations
+            distance, suggestion = model.kneighbors(book_pivot.iloc[book_id, :].values.reshape(1, -1), n_neighbors=11)
 
             poster_url = self.fetch_poster(suggestion)
-            
+            # Return 10 recommendations, skip the first since it's the queried book itself
             for i in range(len(suggestion)):
-                    books = book_pivot.index[suggestion[i]]
-                    for j in books:
-                        books_list.append(j)
-            return books_list , poster_url   
-        
+                books = book_pivot.index[suggestion[i]]
+                for j in books:
+                    books_list.append(j)
+            return books_list[1:11], poster_url[1:11]  # Take the top 10, skipping the selected book
         except Exception as e:
             raise AppException(e, sys) from e
-
 
     def train_engine(self):
         try:
@@ -72,31 +69,18 @@ class Recommendation:
         except Exception as e:
             raise AppException(e, sys) from e
 
-    
-    def recommendations_engine(self,selected_books):
+    def recommendations_engine(self, selected_books):
         try:
-            recommended_books,poster_url = self.recommend_book(selected_books)
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                st.text(recommended_books[1])
-                st.image(poster_url[1])
-            with col2:
-                st.text(recommended_books[2])
-                st.image(poster_url[2])
-
-            with col3:
-                st.text(recommended_books[3])
-                st.image(poster_url[3])
-            with col4:
-                st.text(recommended_books[4])
-                st.image(poster_url[4])
-            with col5:
-                st.text(recommended_books[5])
-                st.image(poster_url[5])
+            recommended_books, poster_url = self.recommend_book(selected_books)
+            st.subheader("🔟 Recommended Books for You:")
+            cols = st.columns(10)
+            for idx in range(10):
+                with cols[idx]:
+                    st.text(recommended_books[idx])
+                    st.image(poster_url[idx])
         except Exception as e:
+            st.error("Error generating recommendations!")
             raise AppException(e, sys) from e
-
-
 
 if __name__ == "__main__":
     st.header('End to End Books Recommender System')
@@ -104,15 +88,17 @@ if __name__ == "__main__":
 
     obj = Recommendation()
 
-    #Training
+    # Training
     if st.button('Train Recommender System'):
         obj.train_engine()
 
-    book_names = pickle.load(open(os.path.join('templates','book_names.pkl') ,'rb'))
+    book_names = pickle.load(open(os.path.join('templates', 'book_names.pkl'), 'rb'))
     selected_books = st.selectbox(
         "Type or select a book from the dropdown",
-        book_names)
-    
-    #recommendation
+        book_names
+    )
+
+    # Show 10 recommendations
     if st.button('Show Recommendation'):
         obj.recommendations_engine(selected_books)
+        
