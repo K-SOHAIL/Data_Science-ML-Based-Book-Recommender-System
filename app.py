745c2a99f93336079ee66e12a0ff17dfa55d172b
K-SOHAIL <kamransohail8910@gmail.com>
2025-11-23 18:02:55 +0530

    updated the app file

--- app.py
+++ app.py
@@ -1,118 +1,61 @@
 import os
 import sys
-import pickle
-import streamlit as st
-import numpy as np
+from six.moves import urllib
+import zipfile
 from books_recommender.logger.log import logging
-from books_recommender.config.configuration import AppConfiguration
-from books_recommender.pipeline.training_pipeline import TrainingPipeline
 from books_recommender.exception.exception_handler import AppException
+from books_recommender.config.configuration import AppConfiguration
 
+class DataIngestion:
 
-class Recommendation:
-    def __init__(self,app_config = AppConfiguration()):
+    def __init__(self, app_config=None):
+        """
+        DataIngestion Initialization
+        """
         try:
-            self.recommendation_config= app_config.get_recommendation_config()
+            logging.info(f"{'='*20}Data Ingestion log started.{'='*20}")
+            if app_config is None:
+                app_config = AppConfiguration()
+            self.data_ingestion_config = app_config.get_data_ingestion_config()
         except Exception as e:
             raise AppException(e, sys) from e
 
-
-    def fetch_poster(self,suggestion):
+    def download_data(self):
+        """
+        Fetch the data from the URL
+        """
         try:
-            book_name = []
-            ids_index = []
-            poster_url = []
-            book_pivot =  pickle.load(open(self.recommendation_config.book_pivot_serialized_objects,'rb'))
-            final_rating =  pickle.load(open(self.recommendation_config.final_rating_serialized_objects,'rb'))
-
-            for book_id in suggestion:
-                book_name.append(book_pivot.index[book_id])
-
-            for name in book_name[0]: 
-                ids = np.where(final_rating['title'] == name)[0][0]
-                ids_index.append(ids)
-
-            for idx in ids_index:
-                url = final_rating.iloc[idx]['image_url']
-                poster_url.append(url)
-
-            return poster_url
-        
-        except Exception as e:
-            raise AppException(e, sys) from e
-        
-
-
-    def recommend_book(self,book_name):
-        try:
-            books_list = []
-            model = pickle.load(open(self.recommendation_config.trained_model_path,'rb'))
-            book_pivot =  pickle.load(open(self.recommendation_config.book_pivot_serialized_objects,'rb'))
-            book_id = np.where(book_pivot.index == book_name)[0][0]
-            distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
-
-            poster_url = self.fetch_poster(suggestion)
-            
-            for i in range(len(suggestion)):
-                    books = book_pivot.index[suggestion[i]]
-                    for j in books:
-                        books_list.append(j)
-            return books_list , poster_url   
-        
+            dataset_url = self.data_ingestion_config.dataset_download_url
+            zip_download_dir = self.data_ingestion_config.raw_data_dir
+            os.makedirs(zip_download_dir, exist_ok=True)
+            data_file_name = os.path.basename(dataset_url)
+            zip_file_path = os.path.join(zip_download_dir, data_file_name)
+            logging.info(f"Downloading data from {dataset_url} into file {zip_file_path}")
+            urllib.request.urlretrieve(dataset_url, zip_file_path)
+            logging.info(f"Downloaded data from {dataset_url} into file {zip_file_path}")
+            return zip_file_path
         except Exception as e:
             raise AppException(e, sys) from e
 
-
-    def train_engine(self):
+    def extract_zip_file(self, zip_file_path: str):
+        """
+        Extracts the zip file into the data directory
+        """
         try:
-            obj = TrainingPipeline()
-            obj.start_training_pipeline()
-            st.text("Training Completed!")
-            logging.info(f"Recommended successfully!")
+            ingested_dir = self.data_ingestion_config.ingested_dir
+            os.makedirs(ingested_dir, exist_ok=True)
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.extractall(ingested_dir)
+            logging.info(f"Extracting zip file: {zip_file_path} into dir: {ingested_dir}")
         except Exception as e:
             raise AppException(e, sys) from e
 
+    def initiate_data_ingestion(self):
+        try:
+            zip_file_path = self.download_data()
+            self.extract_zip_file(zip_file_path=zip_file_path)
+            logging.info(f"{'='*20}Data Ingestion log completed.{'='*20}\n\n")
+        except Exception as e:
+            raise AppException(e, sys) from e
+        return self.data_ingestion_config.ingested_dir  
     
-    def recommendations_engine(self,selected_books):
-        try:
-            recommended_books,poster_url = self.recommend_book(selected_books)
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                st.text(recommended_books[1])
-                st.image(poster_url[1])
-            with col2:
-                st.text(recommended_books[2])
-                st.image(poster_url[2])
-
-            with col3:
-                st.text(recommended_books[3])
-                st.image(poster_url[3])
-            with col4:
-                st.text(recommended_books[4])
-                st.image(poster_url[4])
-            with col5:
-                st.text(recommended_books[5])
-                st.image(poster_url[5])
-        except Exception as e:
-            raise AppException(e, sys) from e
-
-
-
-if __name__ == "__main__":
-    st.header('End to End Books Recommender System')
-    st.text("This is a collaborative filtering based recommendation system!")
-
-    obj = Recommendation()
-
-    #Training
-    if st.button('Train Recommender System'):
-        obj.train_engine()
-
-    book_names = pickle.load(open(os.path.join('templates','book_names.pkl') ,'rb'))
-    selected_books = st.selectbox(
-        "Type or select a book from the dropdown",
-        book_names)
-    
-    #recommendation
-    if st.button('Show Recommendation'):
-        obj.recommendations_engine(selected_books)
